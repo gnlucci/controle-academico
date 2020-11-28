7cd55343fcad477759932ffbbeed153e14258782
gnlucci <75186988+gnlucci@users.noreply.github.com>
2020-11-28 20:21:57 -0300

    Update disciplina.php
    Percebi que não estava funcionando, corrigi aqui.
    Obrigado por compartilhar o codigo.

--- services/insert/disciplina.php
+++ services/insert/disciplina.php
@@ -1,10 +1,12 @@
 <?php
-include '../../db.php';
+include '../../config_db.php';
 
 $nome = $_POST['nome'];
 $curso = $_POST['curso'];
+$carga = $_POST['carga'];
+$ementa = $_POST['ementa'];
 
-$query = "INSERT INTO DISCIPLINAS VALUES( NULL, '$nome', '$curso' )";
-
+//$query = "INSERT INTO DISCIPLINAS VALUES( NULL, '$nome', '$curso')";
+$query = "INSERT INTO DISCIPLINAS VALUES( NULL, '$nome', '$curso', '$carga', '$ementa' ) ";
 mysqli_query($conexao, $query);
 header('location:'.$_SERVER['HTTP_REFERER']);
